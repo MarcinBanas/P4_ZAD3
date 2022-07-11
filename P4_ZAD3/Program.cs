c5d8c7b3fba078639be4f1c81e17af715f41487c
MarcinBanas <mb056625@student.ath.edu.pl>
2022-07-11 11:25:18 +0200

    Dodaj pliki projektów.

---
+++ P4_ZAD3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using P4_ZAD3;
+
+var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+using SqlConnection connection = new SqlConnection(connectionString) ;
+
+
+var regions = connection.Query<Region>("Select * from Region");
+
+foreach (var item in regions)
+{
+    Console.WriteLine($"{item.RegionId}: {item.RegionDescription}");
+}
+
+var query = connection.Query<Territories, Region, Territories>(@"SELECT * FROM Territories t JOIN Region r  ON t.RegionID = r.RegionID WHERE t.RegionID LIKE @regionNr",
+    (territories, region) =>
+    {
+        territories.Region = region;
+        return territories;
+    },
+    new { regionNr = "1" },
+    splitOn: "RegionID");
+
+
+foreach (var item in query)
+{
+    Console.WriteLine($"{ item.territoryDescription } RegionID: {item.Region.RegionId}");
+}
---
+++ P4_ZAD3/Territories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_ZAD3
+{
+    internal class Territories
+    {
+        public int regionid { get; set; }
+        public int territoryID { get; set; }
+        public string territoryDescription { get; set; }
+
+        public Region Region { get; set; }
+    }
+}
